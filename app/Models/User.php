d15304e05a0780a023519a3f9e1a54322aae417f
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2016-05-07 10:18:19 +0700

    Update Entrust classes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Authenticatable
 {
-    use EntrustUserTrait; // add this trait to your user model
+    use EntrustUserTrait;
     /**
      * The attributes that are mass assignable.
      *
